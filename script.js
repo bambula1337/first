a8b1229590f0b7228e0f440a19b944caea309716
Adm's <lemopidr1@gmail.com>
2021-08-07 20:45:52 +0200

    07 08 2021 day - done, сделал первую практику и еще прошел 3 урока по условиям и циклам

--- script.js
+++ script.js
@@ -36,22 +36,3 @@
 //     console.log(i);
 
 // }
-=======
-let numberoffilms = prompt ("сколько фильмов?", "");
-
-const pers = {
-    count: numberoffilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-
-};
-const x = prompt("One of the last films"),
-    y = prompt("Rate"),
-    z = prompt("One of the last films"),
-    e = prompt("Rate");   
-pers.movies[x] = y;
-pers.movies[z] = e;
-console.log(pers);
->>>>>>> b4f24de93c3ac7c5b60d8cab3505352f674f21f3
