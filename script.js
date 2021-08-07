5b872c2cdc1ba39a6ed92dd7d1a0c1850827cc36
Adm's <lemopidr1@gmail.com>
2021-08-07 19:57:47 +0200

    Первое тестовое задание - done

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+// DZ 1
+
+// let numberoffilms = prompt ("сколько фильмов?", "");
+
+// const pers = {
+//     count: numberoffilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+
+// };
+// const x = prompt("One of the last films"),
+//     y = prompt("Rate"),
+//     z = prompt("One of the last films"),
+//     e = prompt("Rate");   
+// pers.movies[x] = y;
+// pers.movies[z] = e;
+// console.log(pers);
+
+// let x = 50;
+// while(x > 45){
+//     console.log(x);
+//     x--;
+// }
+
+// let x = 50;
+
+// for (let i=0; i <= 8; i++){
+//     if(i==6){
+//         continue; 
+//     }
+//     console.log(i);
+
+// }
